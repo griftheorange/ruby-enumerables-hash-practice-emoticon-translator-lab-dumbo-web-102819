629405fd8b5cd9a7572aa23f55c2479c00673395
Griffin Poole <griffinpoole5@gmail.com>
2019-10-12 01:01:25 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -2,6 +2,15 @@ require "yaml"
 
 def load_library(path)
   file = YAML.load_file(path)
+  emoticons = {
+    :get_meaning => {
+      file.reduce({}){|memo, (meanings, english_then_japanese_emoji_array)|
+        memo[english_then_japanese_emoji_array[1]] = meanings
+      }
+    },
+    :get_emoticon =>
+  }
+  
 end
 
 def get_japanese_emoticon
