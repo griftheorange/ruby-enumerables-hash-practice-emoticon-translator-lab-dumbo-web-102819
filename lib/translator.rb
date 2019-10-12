6729f1146f7d6618e889f7c0cdd50f5c76f0a29c
Griffin Poole <griffinpoole5@gmail.com>
2019-10-12 00:44:54 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -3,7 +3,7 @@ require "yaml"
 def load_library(path)
   emoticons = YAML.load_file(path)
   emoticons[:get_meaning] = []
-  emoticons[:get_emoticons] = []
+  emoticons[:get_emoticon] = []
 end
 
 def get_japanese_emoticon
